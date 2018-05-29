fc9afc3f53d13f4a03e111c7b43f753a4ebb5cbf
Tarcisio Ferreira <junior.tarcisio@gmail.com>
2018-05-29 01:14:58 -0300

    Added album-detail page

--- src/app/lessons/world-albums-detail/world-albums-detail.component.ts
+++ src/app/lessons/world-albums-detail/world-albums-detail.component.ts
@@ -1,4 +1,7 @@
+import { getTestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { VocabularyService } from '../../shared/services/vocabulary.service';
 
 @Component({
   selector: 'app-world-albums-detail',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorldAlbumsDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private _vocabularyService : VocabularyService) { }
+
+  private album : any;
 
   ngOnInit() {
+    const albumName = this.route.snapshot.params['album'];
+    this.album = this._vocabularyService.getWordAlbums().find(x => { 
+      var xAlbum = x.name.replace(' ','').replace(' ','').replace('&','');
+      return xAlbum == albumName ;
+    });
+    console.log(this.album);
   }
-
 }
